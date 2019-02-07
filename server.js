d7941bde8d5d76fa0507b161e48885dbfabaad18
myrean <dhughes1@uci.edu>
2019-02-07 12:57:11 -0800

    server online

---
+++ server.js
@@ -0,0 +1,37 @@
+//Dependencies
+//=================================
+var express = require("express");
+var path = require("path");
+// var Table = require("./table");
+
+
+//Main
+//=========================================
+// Sets up the Express App
+var app = express();
+//dynamic port assign
+var PORT = process.env.PORT || 8080;
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+//Routes
+//=========================================
+    // Basic route that sends the user first to the AJAX Page
+    app.get("/", function (req, res) {
+    
+        res.sendFile(path.join(__dirname, "./app/public/home.html"));
+    });
+
+
+    // Starts the server to begin listening
+    // =============================================================
+    app.listen(PORT, function () {
+        console.log("App listening on PORT " + PORT);
+    })
+
+
+//Functions
+//=========================================
