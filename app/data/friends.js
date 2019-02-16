4b14328c1e5e571056a1fb2a4429294cf760605d
myrean <dhughes1@uci.edu>
2019-02-15 20:50:17 -0800

    upd total

--- app/data/friends.js
+++ app/data/friends.js
@@ -22,7 +22,7 @@ var makeScores = require("./makeScores");
 // Example Friends with names etc
 var james = new Friend("James Donovan Halliday", "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjyvZvw_q3gAhVR71QKHb1SDlsQjRx6BAgBEAU&url=https%3A%2F%2Freadyplayerone.fandom.com%2Fwiki%2FJames_Donovan_Halliday&psig=AOvVaw3xb8Sy3jNeyZK6qFZUceks&ust=1549778946193368", [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]);
 
-var indigo = new Friend("Indigo Montoya", "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.ragan.com%2Fwp-content%2Fuploads%2F2017%2F09%2FInigo_Montoya_at_Work.jpg&imgrefurl=https%3A%2F%2Fwww.ragan.com%2F8-reasons-you-should-be-more-like-inigo-montoya-at-work%2F&docid=rEV1QPl3CpMr4M&tbnid=VDqbWeM1VFl5DM%3A&vet=10ahUKEwj0nerAqbDgAhU9JDQIHdWHBVYQMwhqKAAwAA..i&w=600&h=400&bih=713&biw=990&q=inigo%20montoya&ved=0ahUKEwj0nerAqbDgAhU9JDQIHdWHBVYQMwhqKAAwAA&iact=mrc&uact=8", [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]);
+var indigo = new Friend("Indigo Montoya", "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.ragan.com%2Fwp-content%2Fuploads%2F2017%2F09%2FInigo_Montoya_at_Work.jpg&imgrefurl=https%3A%2F%2Fwww.ragan.com%2F8-reasons-you-should-be-more-like-inigo-montoya-at-work%2F&docid=rEV1QPl3CpMr4M&tbnid=VDqbWeM1VFl5DM%3A&vet=10ahUKEwj0nerAqbDgAhU9JDQIHdWHBVYQMwhqKAAwAA..i&w=600&h=400&bih=713&biw=990&q=inigo%20montoya&ved=0ahUKEwj0nerAqbDgAhU9JDQIHdWHBVYQMwhqKAAwAA&iact=mrc&uact=8", [ 4, 10, 4, 6, 7, 7, 2, 5, 8, 8 ]);
 
 var hugh = new Friend("Hugh Grant", "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.biography.com%2F.image%2Ft_share%2FMTE5NTU2MzE2MjM2NjQ1ODk5%2Fhugh-grant-9318171-1-402.jpg&imgrefurl=https%3A%2F%2Fwww.biography.com%2Fpeople%2Fhugh-grant-9318171&docid=10yUpee38Q9UUM&tbnid=2ClTiOkudJsXFM%3A&vet=10ahUKEwjQmquPqrDgAhU8GTQIHYGhDhsQMwhsKAEwAQ..i&w=1200&h=1200&bih=713&biw=990&q=hugh%20grant&ved=0ahUKEwjQmquPqrDgAhU8GTQIHYGhDhsQMwhsKAEwAQ&iact=mrc&uact=8", [3, 10, 6, 5, 3, 4, 1, 10, 6, 9]);
 
@@ -30,7 +30,7 @@ var charlize = new Friend("Charlize Theron", "https://www.google.com/imgres?imgu
 
 var amy = new Friend("Amy Schumer", "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.bwwstatic.com%2Fheadshots%2F402388sm.jpg&imgrefurl=https%3A%2F%2Fwww.broadwayworld.com%2Fpeople%2Fheadshot%2FAmy-Schumer%2F&docid=iZlwZR7B2WY_WM&tbnid=alLDZtRyLldSEM%3A&vet=10ahUKEwi4qM22qrDgAhXeGDQIHRreByYQMwhNKAAwAA..i&w=204&h=254&bih=713&biw=990&q=amy%20schumer%20headshot&ved=0ahUKEwi4qM22qrDgAhXeGDQIHRreByYQMwhNKAAwAA&iact=mrc&uact=8", [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]);
 
-var kate = new Friend("Kate Winslet", "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fd3%2F58%2F83%2Fd35883dc43d7f48e1b7825245844fcde.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F236227942929717166%2F&docid=M9PuQgKg1EYKdM&tbnid=tWNvpmashKk5wM%3A&vet=10ahUKEwjNoOjSqrDgAhU6JzQIHRiJCi8QMwhNKAEwAQ..i&w=500&h=588&bih=713&biw=990&q=kate%20winslet%20headshot&ved=0ahUKEwjNoOjSqrDgAhU6JzQIHRiJCi8QMwhNKAEwAQ&iact=mrc&uact=8", [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]);
+var kate = new Friend("Kate Winslet", "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fd3%2F58%2F83%2Fd35883dc43d7f48e1b7825245844fcde.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F236227942929717166%2F&docid=M9PuQgKg1EYKdM&tbnid=tWNvpmashKk5wM%3A&vet=10ahUKEwjNoOjSqrDgAhU6JzQIHRiJCi8QMwhNKAEwAQ..i&w=500&h=588&bih=713&biw=990&q=kate%20winslet%20headshot&ved=0ahUKEwjNoOjSqrDgAhU6JzQIHRiJCi8QMwhNKAEwAQ&iact=mrc&uact=8", [ 6, 6, 3, 10, 10, 2, 3, 3, 6, 2 ]);
 
 
 //EXTRA --  Friend Generator --- Generate a Friend --- Cool!
@@ -79,11 +79,3 @@ var newFriend = new Friend("New Friend", "#", [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]);
 
 //export the dataObj
 module.exports = dataObj;
-
-
-
-
-
-
-
-
