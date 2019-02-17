6529fe6b238ec63a8c2d63edd0bf929e50c3110e
myrean <dhughes1@uci.edu>
2019-02-16 19:37:27 -0800

    added diff to obj

--- app/data/Friend.js
+++ app/data/Friend.js
@@ -1,10 +1,11 @@
 //Functions
 //=========================================
-var Friend = function(name, photo, randomRes, totalScore) {
+var Friend = function(name, photo, randomRes, totalScore, diff) {
     this.name = name;
     this.photo = photo;
     this.randomRes = randomRes;
     this.totalScore = totalScore;
+    this.diff = diff;
 
 }
 
