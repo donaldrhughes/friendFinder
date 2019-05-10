91e74c1481d82f40db291397f8081657c59785ac
myrean <dhughes1@uci.edu>
2019-05-10 15:34:55 -0700

    fixed format and scoring randomization for results

--- app/data/makeScores.js
+++ app/data/makeScores.js
@@ -1,9 +1,11 @@
 
 //EXTRA function which returns the results of a Generated 10 question Array
 var makeScores = function makeScores() {
-    var result = Array.from({ length: 10 }, () => Math.floor(Math.random() * 10) + 1);;
+    var result = Array.from({ length: 10 }, () => Math.floor(Math.random() * 5) + 1);;
 
     return result;
 }
-//Build a Giphy Response that pulls a random image
+
+
+//Build a Giphy Response that pulls a random image - Not completed
 module.exports = makeScores;
