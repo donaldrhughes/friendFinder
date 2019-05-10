a853523b96a02398dbc2800e49cda53d133fd4e0
myrean <dhughes1@uci.edu>
2019-05-10 16:19:02 -0700

    upd logic

--- app/data/makeScores.js
+++ app/data/makeScores.js
@@ -1,7 +1,7 @@
 
 //EXTRA function which returns the results of a Generated 10 question Array
 var makeScores = function makeScores() {
-    var result = Array.from({ length: 10 }, () => Math.floor(Math.random() * 5) + 1);;
+    var result = Array.from({ length: 10 }, () => Math.floor(Math.random() * 10) + 1);;
 
     return result;
 }
